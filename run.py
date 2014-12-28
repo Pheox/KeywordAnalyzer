276ac3e2f8f8a8b1cfa9749080feefd808ef7f4a
Vladimir Brigant <vladimir.brigant@acision.com>
2014-12-28 22:38:53 +0100

    port in run[p].py scripts parametrized

--- run.py
+++ run.py
@@ -1,4 +1,16 @@
 #!flask/bin/python
 
+import sys
+
 from app import app
-app.run(debug = True, threaded=True)
+
+
+port = 5000
+print sys.argv
+
+if len(sys.argv) >= 2:
+	port = int(sys.argv[1])
+
+
+app.run(debug = True, threaded=True, port=port)
+
